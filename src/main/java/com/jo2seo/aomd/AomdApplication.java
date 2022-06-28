454f00d5de2c49ea8e2886f38229a2ca2bf4fa0b
thuthi_notebook <whquddn55@gmail.com>
2022-06-28 22:26:12 +0900

    [Init]

---
+++ src/main/java/com/jo2seo/aomd/AomdApplication.java
@@ -0,0 +1,13 @@
+package com.jo2seo.aomd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AomdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AomdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jo2seo/aomd/AomdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo2seo.aomd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AomdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
