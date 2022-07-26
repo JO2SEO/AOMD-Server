3db59a8cd620a5837ac7c5e1da34eddf2b7412a1
조병우 <whquddn55@gmail.com>
2022-07-26 19:34:37 +0900

    Feature/auth (#3)
    * [Update]: CallbackUrl을 Origin에서 찾을 수 있도록 수정
    
    * Update: FileService를 통해서 카카오 이미지 저장하도록 변경

--- src/main/java/com/jo2seo/aomd/auth/AuthController.java
+++ src/main/java/com/jo2seo/aomd/auth/AuthController.java
@@ -9,17 +9,20 @@ import com.jo2seo.aomd.auth.dto.response.KakaoLoginResponse;
 import com.jo2seo.aomd.security.JwtFilter;
 import com.jo2seo.aomd.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
     private final AuthService authService;
     private final TokenProvider tokenProvider;
@@ -43,11 +46,18 @@ public class AuthController {
         System.out.println("code = " + code);
     }
 
+    /* TODO: request에서 callbackUrl제거 */
     @PostMapping("/auth/kakao")
     public ResponseEntity<BaseResponse> kakaoLogin(
+            HttpServletRequest request,
             @Valid @RequestBody KakaoLoginRequest kakaoLoginRequest
-    ) throws JsonProcessingException {
+    ) throws IOException {
         if (kakaoLoginRequest.getCode() == null) return new ResponseEntity(new BaseResponse(BaseResponseStatus.DATABASE_ERROR), HttpStatus.BAD_REQUEST);
+        String origin = request.getHeader("origin");
+        if (origin == null) return new ResponseEntity(new BaseResponse(BaseResponseStatus.DATABASE_ERROR), HttpStatus.BAD_REQUEST);
+
+        String callbackUrl = origin + "/AOMD-Client/oauth";
+        log.debug(callbackUrl);
 
         KakaoLoginResponse kakaoLoginResponse = authService.kakaoLogin(kakaoLoginRequest.getCode(), kakaoLoginRequest.getCallbackUrl());
         String jwt = tokenProvider.createToken(kakaoLoginResponse.getEmail(), kakaoLoginResponse.getPassword());
