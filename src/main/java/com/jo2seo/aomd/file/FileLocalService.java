78ad442a71d6bd73b6431837d87dfb726f1fd313
조병우 <whquddn55@gmail.com>
2022-07-26 19:10:26 +0900

    Update: FileUtil을 FileService로 변경
    - static class였던 Util을 Service로 변경하여 다형성 활용할 수 있도록 수정

--- src/main/java/com/jo2seo/aomd/file/FileLocalService.java
+++ src/main/java/com/jo2seo/aomd/file/FileLocalService.java
@@ -1,5 +1,6 @@
 package com.jo2seo.aomd.file;
 
+import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,10 +11,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.UUID;
 
-public class FileUploadUtil {
+@Service
+public class FileLocalService implements FileService {
     public static final String PROJECT_DIR = System.getProperty("user.dir");
     public static final String PROFILE_IMG_DIR = "profileImg";
-    public static String saveProfileImage(MultipartFile file) throws IOException {
+    @Override
+    public String saveProfileImage(MultipartFile file) throws IOException {
         final String destinationDir = PROJECT_DIR + "/" + PROFILE_IMG_DIR;
 
         String sourceFileName = file.getOriginalFilename();
@@ -28,7 +31,8 @@ public class FileUploadUtil {
         return destinationFile.getPath();
     }
 
-    public static String downloadKakaoProfileImage(String profileImgUrl) throws IOException {
+    @Override
+    public String downloadKakaoProfileImage(String profileImgUrl) throws IOException {
         final String destinationDir = PROJECT_DIR + "/" + PROFILE_IMG_DIR;
 
         String sourceFileExtension = StringUtils.getFilenameExtension(profileImgUrl).toLowerCase();
