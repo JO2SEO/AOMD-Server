6bf9f9f0f341e7e49b1b57b2d8d7704dd30edf95
조병우 <whquddn55@gmail.com>
2022-07-27 23:37:51 +0900

    Add: Resume Entity 추가

---
+++ src/main/java/com/jo2seo/aomd/resume/Resume.java
@@ -0,0 +1,27 @@
+package com.jo2seo.aomd.resume;
+
+import com.jo2seo.aomd.portfolio.Portfolio;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@NoArgsConstructor
+public class Resume {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "resume_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+
+    private String question = "";
+
+    @Lob
+    private String content = "";
+}
