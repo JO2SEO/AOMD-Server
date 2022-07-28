d1c241d0eb17ad62cb7b18e44c27bbec1f6993ad
조병우 <whquddn55@gmail.com>
2022-07-28 13:25:09 +0900

    Add: PortfolioChainOrder entity 추가
    - 포트폴리오의 chain ordering을 위한 entity 추가

---
+++ src/main/java/com/jo2seo/aomd/portfolio/PortfolioChainOrder.java
@@ -0,0 +1,22 @@
+package com.jo2seo.aomd.portfolio;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class PortfolioChainOrder {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "portfolio_chain_order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+
+    private String chainId;
+    private Integer orderIndex;
+}
